0f6380747d403726d3e869057750ec9787d21376
Alexa Karp <alexakarp@hotmail.com>
2022-06-08 03:01:31 -0500

    commit tree ec3ab7b185a314754948b9306b8098912d636a80

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (spy) {
+    spy()
+};
+
+function returnsANamedFunction() {
+    return function multiply2(num1) {
+        (num1*2);
+    }
+};
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log('Anonymous Function');
+    }
+};
